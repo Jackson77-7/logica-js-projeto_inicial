a1e272a5ddb6bc540e476a8c39a4939b950fb6cf
Jackson77-7 <165019762+Jackson77-7@users.noreply.github.com>
2024-03-26 11:22:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+// aparece pro usuario uma janela de alerta
 alert("bom dia, bem vindo ao jogo")
 
-let numeroSecreto = 33;
-let numeroEscolhido = parseInt(prompt("digite um numero de 1' a 50"));
+//let é usado pra declarar uma variavel
+let numeroSecreto = Math.floor(Math.random() *51)
+//parceint para converter numero para inteiro, prompt para caixa do navegador
 
-if(numeroSecreto == numeroEscolhido){
-    alert("parabens voce acertou")
-    }else if(numeroEscolhido > numeroSecreto){
-        alert("este numero é maior do que o numero secreto")
-    }else{
         alert("Este numero é menor do que o numero secreto")
-    };
+let numeroTentativas = 0;
+
+//if eu uso para testar uma logica "==" para sinal, "> ou <" maior ou mneor ">="" ou "<="
+//else é usado par ao se não, ou seja, quando um teste logico
+
+    while (numeroTentativas <= 6) {
+        let numeroEscolhido = parseInt(prompt("digite um numero de 0 a 50"));
+        if(numeroSecreto == numeroEscolhido){
+            alert(`parabéns você acertou o numero ${numeroSecreto} `)
+        }else if(numeroEscolhido > numeroSecreto) {
+            alert(`voce digitou ${numeroEscolhido} que é um numero maior que o numero secreto`)
+        }else{
+            alert(`voce digitou ${numeroEscolhido} que é um numero menor que o numero secreto`)
+        }
+        numeroTentativas = numeroTentativas +1;
+    } 
