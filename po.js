f17afd2f7b805082f54db629de75f07da745fbcd
Jackson77-7 <165019762+Jackson77-7@users.noreply.github.com>
2024-04-09 07:38:25 -0300

    Update po.js

--- po.js
+++ po.js
@@ -1,3 +1,20 @@
+function gerarTabela() {
+    let linhas = parseInt(prompt("digite o numero de linhas da sua tabela"))
+
+
+    let colunas = parseInt(prompt("digite o numero de colunas da sua tabela"))
+
+    let tabela = []
+
+    for (let i = 0; i < linhas; i++) {
+        tabela[i] = [];
+        for (let j = 0; j < colunas; j++) {
+            tabela[i][j] = gerarNumeroAleatorio(100)
+
+        }
+    }
+    return tabela
+}
 let tabelaTresPorTres = [
     [0, 0, 0],
     [0, 0, 0],
@@ -17,4 +34,4 @@ function gerarNumeroAleatorio(range) {
     return Math.floor(Math.random() * range + 1);
 
 }
-console.table(aleatorizarTabela(tabelaTresPorTres));
+console.table(gerarTabela());
